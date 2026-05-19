b8c500fbfcb9314ab7f50823029a5032193e669e
sandeep <sandeepmishra898k@gmail.com>
2026-05-19 17:37:22 +0530

    UC1 – Calculate Length of a Line

--- App/src/LineComparisonComputation.java
+++ App/src/LineComparisonComputation.java
@@ -2,6 +2,13 @@ public class LineComparisonComputation {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Line Comparison Computation Program");
+        int x1 = 2, y1 = 4;
+        int x2 = 6, y2 = 8;
+
+        double length = Math.sqrt(
+                Math.pow((x2 - x1), 2) +
+                        Math.pow((y2 - y1), 2));
+
+        System.out.println("Length of Line = " + length);
     }
 }
